b74e3a8f8b0380d6f5457715d3e20c7cac17894d
zhengyutay <tayzhengyu@gmail.com>
2022-04-27 16:15:20 +0800

    fix build

--- src/views/JupiterForm/FeeInfo.tsx
+++ src/views/JupiterForm/FeeInfo.tsx
@@ -2,32 +2,38 @@ import React, { FunctionComponent, useEffect, useState } from "react";
 
 import { RouteInfo, TransactionFeeInfo } from "@jup-ag/react-hook";
 
-const FeeInfo: FunctionComponent<{ route: RouteInfo }> = ({ route }: { route: RouteInfo }) => {
-    const [state, setState] = useState<TransactionFeeInfo>();
-    useEffect(() => {
-        setState(undefined);
-        route.getDepositAndFee().then(setState);
-    }, [route]);
-    return (
+const FeeInfo: FunctionComponent<{ route: RouteInfo }> = ({
+  route,
+}: {
+  route: RouteInfo;
+}) => {
+  const [state, setState] = useState<TransactionFeeInfo>();
+  useEffect(() => {
+    setState(undefined);
+    route.getDepositAndFee().then(setState);
+  }, [route]);
+  return (
+    <div>
+      {state && (
         <div>
-            {state && (
-                <div>
-                    <br />
-                    Deposit For serum: {/* In lamports */}
-                    {state.openOrdersDeposits.reduce((total, i) => total + i, 0) /
-                        10 ** 9}{" "}
-                    SOL
-                    <br />
-                    Deposit For ATA: {/* In lamports */}
-                    {state.ataDeposit / 10 ** 9} SOL
-                    <br />
-                    Fee: {/* In lamports */}
-                    {state.signatureFee / 10 ** 9} SOL
-                    <br />
-                </div>
-            )}
+          <br />
+          Deposit For serum: {/* In lamports */}
+          {state.openOrdersDeposits.reduce((total, i) => total + i, 0) /
+            10 ** 9}{" "}
+          SOL
+          <br />
+          Deposit For ATA: {/* In lamports */}
+          {state.ataDeposits.reduce((total, deposit) => total + deposit, 0) /
+            10 ** 9}{" "}
+          SOL
+          <br />
+          Fee: {/* In lamports */}
+          {state.signatureFee / 10 ** 9} SOL
+          <br />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
-export default FeeInfo
+export default FeeInfo;
